9e2dcaf1084ec2d328acbb4b2d1d1aaccb3d2a43
fattoot <37107385+fattoot@users.noreply.github.com>
2018-03-19 07:41:38 +0800

    Update install_seafile.sh

--- install_seafile.sh
+++ install_seafile.sh
@@ -22,10 +22,10 @@ function chk_firewall() {
 #安装seafile函数
 function install_sea() {
 	cd /home/MyCloud
-	#下载安装包6.2.3 64bit
-	wget http://seafile-downloads.oss-cn-shanghai.aliyuncs.com/seafile-server_6.2.3_x86-64.tar.gz
+	#下载安装包6.2.5 64bit
+	wget http://seafile-downloads.oss-cn-shanghai.aliyuncs.com/seafile-server_6.2.5_x86-64.tar.gz
 	#解压
-	tar -zxvf seafile-server_6.2.3_x86-64.tar.gz
+	tar -zxvf seafile-server_6.2.5_x86-64.tar.gz
 	mkdir installed
 	mv seafile-server*.tar.gz ./installed
 	mv seafile-server-6* seafile-server
